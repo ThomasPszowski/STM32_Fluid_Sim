e8042df003a801e6e9cb7a3bf0751e51fb259c43
ThomasPszowski <162191217+ThomasPszowski@users.noreply.github.com>
2025-05-02 13:44:40 +0200

    automatic restart after fail implemented

--- Core/Inc/SimC.h
+++ Core/Inc/SimC.h
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 #define GRID_SIZE 8
-#define REPULSION_GRID_DIM 8
+#define REPULSION_GRID_DIM 10
 #define REPULSION_GRID_SIZE (REPULSION_GRID_DIM * REPULSION_GRID_DIM)
 #define PARTICLE_COUNT 30
 #define DT 0.1f
